6d1f9f0279bd4c6c3f52d0b9ab2f49b9eac5affa
torgeiru <torgeir.skovholt.pro@gmail.com>
2025-06-23 00:34:05 +0200

    Fjernet futex call fra __lockfile

--- src/stdio/__lockfile.c
+++ src/stdio/__lockfile.c
@@ -6,18 +6,12 @@ int __lockfile(FILE *f)
 	int owner = f->lock, tid = __pthread_self()->tid;
 	if ((owner & ~MAYBE_WAITERS) == tid)
 		return 0;
-	owner = a_cas(&f->lock, 0, tid);
-	if (!owner) return 1;
-	while ((owner = a_cas(&f->lock, 0, tid|MAYBE_WAITERS))) {
-		if ((owner & MAYBE_WAITERS) ||
-		    a_cas(&f->lock, owner, owner|MAYBE_WAITERS)==owner)
-			__futexwait(&f->lock, owner|MAYBE_WAITERS, 1);
-	}
+
+	while (a_cas(&f->lock, 0, tid));
 	return 1;
 }
 
 void __unlockfile(FILE *f)
 {
-	if (a_swap(&f->lock, 0) & MAYBE_WAITERS)
-		__wake(&f->lock, 1, 1);
+	a_swap(&f->lock, 0);
 }
